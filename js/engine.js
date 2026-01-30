5a10cb53f0ccdada7bc983fe44d5443728f8e071
romanvartanov <47086312+romanvartanov@users.noreply.github.com>
2026-01-30 12:16:59 +0100

    Update engine.js

--- js/engine.js
+++ js/engine.js
@@ -1,3 +1,22 @@
+import q1 from "../questions/q01.js";
+const QUESTIONS = [q1];
+
+const form = document.getElementById("testForm");
+QUESTIONS.forEach(q => {
+  const div = document.createElement("div");
+  div.className = "question";
+  div.innerHTML = `
+    <p><strong>${q.id.toUpperCase()}</strong><br>${q.text}</p>
+    ${q.answers.map((a,i)=>`
+      <label>
+        <input type="radio" name="${q.id}" value="${i}">
+        ${a.text}
+      </label><br>
+    `).join("")}
+  `;
+  form.appendChild(div);
+});
+
 <script>
 const scores = {
   q1:[[1,-1],[-1,1],[1,1],[-1,-1]],
